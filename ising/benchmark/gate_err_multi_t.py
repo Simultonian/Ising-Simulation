6e2f1566b6475d3c58e6b2c9e191046039ba0d20
Zeeshan Ahmed <zeeshan060801@gmail.com>
2023-11-30 16:07:38 +0530

    y error fixed

--- ising/benchmark/gate_err_multi_t.py
+++ ising/benchmark/gate_err_multi_t.py
@@ -41,8 +41,8 @@ def plot_gate_error(
             result = results[method]
             if ind == 0:
                 sns.lineplot(
-                    y=result,
-                    x=error_points,
+                    x=result,
+                    y=error_points,
                     ax=ax,
                     label=config["label"],
                     color=config["color"],
@@ -50,26 +50,26 @@ def plot_gate_error(
                 )
             else:
                 sns.lineplot(
-                    y=result,
-                    x=error_points,
+                    x=result,
+                    y=error_points,
                     ax=ax,
                     color=config["color"],
                     linestyle=linestyle[ind],
                 )
 
-            sns.scatterplot(y=result, x=error_points, ax=ax, color=config["color"])
+            sns.scatterplot(x=result, y=error_points, ax=ax, color=config["color"])
 
         # ax.text(error_points[-1] * 2, results["taylor"][-1] * 1.6, f"t={time}")
 
-    ax.invert_xaxis()
+    # ax.invert_xaxis()
     ax.set_xscale("log")
     ax.set_yscale("log")
-    ax.set_xlabel(r"$\log_{10}(\epsilon)$")
-    ax.set_ylabel(r"$\log_{10}(\text{gate count})$")
+    ax.set_ylabel(r"$\log_{10}(\epsilon)$")
+    ax.set_xlabel(r"$\log_{10}(\text{gate count})$")
 
     ax.set_title("Gate vs Error for Simulation Techniques")
 
-    ax.legend(loc="upper left", framealpha=1)
+    ax.legend(loc="upper right", framealpha=1)
     # plt.legend()
     diagram_name = "plots/benchmark/gate_count_multi_t.png"
     print(f"Saving diagram at:{diagram_name}")
@@ -83,7 +83,7 @@ if __name__ == "__main__":
     end_err_exp = -3
     point_count = 10
     obs_norm = 1
-    time = [1e5]
+    time = [100]
     scale = [1.5, 1.5]
     plot_gate_error(
         qubit, h_val, start_err_exp, end_err_exp, point_count, obs_norm, time, scale
