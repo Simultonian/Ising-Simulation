79ab899c0c8fd888f3bd108b97ba121ad3315b4f
Zeeshan Ahmed <zeeshan060801@gmail.com>
2023-12-15 21:44:28 +0530

    `LCUSynthesis` WIP

--- ising/groundstate/simulation/run_numerical.py
+++ ising/groundstate/simulation/run_numerical.py
@@ -5,7 +5,6 @@ import numpy as np
 from ising.hamiltonian import parametrized_ising
 from ising.utils import read_input_file
 
-
 from qiskit.quantum_info import Operator
 from qiskit.circuit import Parameter
 
@@ -15,15 +14,23 @@ from ising.utils import read_input_file, close_state
 from ising.simulation.trotter import (
     LieCircuit,
     QDriftCircuit,
-    SparseLie,
     GroupedLieCircuit,
     GroupedQDriftCircuit,
-    TwoQDriftCircuit,
     GSQDriftCircuit,
 )
 from ising.simulation.exact import ExactSimulation
 
-from ising.singlelcu.simulation import GroundState
+from ising.groundstate.simulation.singlelcu import LCUSynthesizer
+
+
+SYNTH_MAP = {
+    "exact": ExactSimulation,
+    "lie": LieCircuit,
+    "qdrift": QDriftCircuit,
+    "grouped_lie": GroupedLieCircuit,
+    "grouped_qdrift": GroupedQDriftCircuit,
+    "gs_qdrift": GSQDriftCircuit,
+}
 
 
 def run_numerical(paras):
@@ -32,28 +39,15 @@ def run_numerical(paras):
         10 ** paras["start_h"], 10 ** paras["end_h"], paras["count_h"]
     )
 
-    method = paras["method"]
+    qubits = map(
+        int, np.linspace(paras["start_qubit"], paras["end_qubit"], paras["qubit_count"])
+    )
 
-    if method == "exact":
-        circuit_synthesis = ExactSimulation
-    elif method == "lie":
-        circuit_synthesis = LieCircuit
-    elif method == "qdrift":
-        circuit_synthesis = QDriftCircuit
-    elif method == "sparse_lie":
-        circuit_synthesis = SparseLie
-    elif method == "grouped_lie":
-        circuit_synthesis = GroupedLieCircuit
-    elif method == "grouped_qdrift":
-        circuit_synthesis = GroupedQDriftCircuit
-    elif method == "two_qdrift_circuit":
-        circuit_synthesis = TwoQDriftCircuit
-    elif method == "gs_qdrift":
-        circuit_synthesis = GSQDriftCircuit
-    else:
-        raise ValueError("Incorrect method:", method)
+    circuit_synthesis = SYNTH_MAP.get(paras["method"])
+    if circuit_synthesis is None:
+        raise ValueError(f"Incorrect synthesis method: {paras['method']}")
 
-    for num_qubit in range(paras["start_qubit"], paras["end_qubit"] + 1):
+    for num_qubit in qubits:
         observable = overall_magnetization(num_qubit)
         h_para = Parameter("h")
         parametrized_ham = parametrized_ising(num_qubit, h_para)
@@ -65,15 +59,15 @@ def run_numerical(paras):
             circuit_manager.subsitute_h(h)
             circuit_manager.construct_parametrized_circuit()
 
-            lcu_run = GroundState(
+            lcu_run = LCUSynthesizer(
                 circuit_manager,
                 observable,
-                eeta=paras["eeta"],
-                eps=paras["eps"],
-                prob=paras["prob"],
+                eeta=paras["overlap"],
+                eps=paras["error"],
+                prob=paras["success"],
             )
 
-            ground_state = circuit_manager.ham_subbed.ground_state
+            ground_state = circuit_manager.ground_state
             init_state = close_state(ground_state, paras["overlap"])
             rho_init = np.outer(init_state, init_state.conj().T)
 
@@ -87,34 +81,30 @@ def run_numerical(paras):
     return qubit_wise_answers
 
 
+def file_numerical(file_name):
+    parameters = read_input_file(file_name)
+
+    results = run_numerical(parameters)
+
+    file_name = f"data/groundstate/{parameters['method']}_{parameters['start_qubit']}_to_{parameters['end_qubit']}.json"
+
+    print(f"Saving results at: {file_name}")
+    with open(file_name, "w") as file:
+        json.dump(results, file)
+
+
 def main():
     parser = argparse.ArgumentParser(
-        description="Overall magnetization of Ising using Qiskit"
+        description="Overall magnetization of Ising using Synthesis."
     )
     parser.add_argument("--input", type=str, help="File for input parameters.")
     args = parser.parse_args()
 
-    parameters = read_input_file(args.input)
-
-    results = run_numerical(parameters)
-
-    file_name = f"data/singlelcu/output/magnetization_num_{parameters['method']}_{parameters['start_qubit']}_to_{parameters['end_qubit']}.json"
-
-    print(f"Saving results at: {file_name}")
-    with open(file_name, "w") as file:
-        json.dump(results, file)
+    file_numerical(args.input)
 
 
 def test_main():
-    parameters = read_input_file("data/singlelcu/input/default.json")
-
-    results = run_numerical(parameters)
-
-    file_name = f"data/singlelcu/output/magnetization_num_{parameters['method']}_{parameters['start_qubit']}_to_{parameters['end_qubit']}.json"
-
-    print(f"Saving results at: {file_name}")
-    with open(file_name, "w") as file:
-        json.dump(results, file)
+    file_numerical("data/input/groundstate.json")
 
 
 if __name__ == "__main__":
--- ising/groundstate/simulation/singlelcu.py
+++ ising/groundstate/simulation/singlelcu.py
@@ -6,84 +6,18 @@ from collections import Counter
 from qiskit.quantum_info import SparsePauliOp
 
 from ising.hamiltonian import Hamiltonian
-from ising.simulation import Synthesizer
 from ising.utils.constants import ONEONE, ZEROZERO, PLUS
 from ising.utils import close_state, MAXSIZE
+from ising.groundstate.simulation.utils import (
+    spectral_norm,
+    calculate_mu,
+    ground_state_constants,
+    calculate_lcu_constants,
+)
 
 
-def spectral_norm(eigenval):
-    """
-    ||O|| = max eigenvalue of (O @ O^T), can be rewritten as the following
-    """
-    max_eigval = max(np.abs(eigenval**2)) ** 0.5
-    return max_eigval
-
-
-def calculate_mu(mu_samples, count, coeffs):
-    """
-    Formula is in the algorithm 1 of the paper
-    """
-    norm_1_sq = np.linalg.norm(coeffs, ord=1) ** 2
-    mu_sum = sum(mu_samples)
-
-    mu = (norm_1_sq * mu_sum) / count
-    return mu
-
-
-def ground_state_constants(spectral_gap, eeta, eps, prob, obs_norm):
-    """
-    Calculating the parameters for the LCU based on the given constants.
-
-    Values calculated:
-        - t: Base time for evolution
-        - count: Number of iterations (T in the paper)
-        - gamma: Error gap between LCU and exact function
-        - delta_t: Time factor (refer paper)
-        - m: M from paper, the max value of j in the loop
-
-    Calculations:
-        - t = (1/delta^2) * log(||O|| / eeta * eps)
-
-        - count = (||O||^2 ln(2/delta)) / (eps^2 * eeta^4)
-
-        - gamma = eps * eeta^2 / 12 ||O||
-
-        - delta_t = 1 / (sqrt(2t) + sqrt(2log(5/gamma)))
-    """
-
-    t = (1 / spectral_gap**2) * np.log2(1 / (eeta * eps))
-    count = int(((obs_norm**2) * np.log(2 / prob)) / (eps**2 * eeta**4))
-
-    gamma = (eps * (eeta**2)) / (12 * obs_norm)
-    m = int(
-        np.ceil(
-            np.sqrt(2)
-            * (np.sqrt(t) + np.sqrt(np.log2(5 / gamma)))
-            * np.sqrt(np.log2(4 / gamma))
-        )
-    )
-    delta_t = 1 / (np.sqrt(2 * t) + np.sqrt(2 * np.log2(5 / gamma)))
-    return {"t": t, "count": count, "gamma": gamma, "m": m, "delta_t": delta_t}
-
-
-def calculate_lcu_constants(m, delta_t, t):
-    """
-    Coefficients are calculated by:
-        c_j = (delta_t / sqrt(2pi)) * e^{-j^2 delta_t^2 / 2}
-
-    The overall time value for given j is calculated by:
-        t_j = j * delta_t * sqrt(2*t)
-    """
-    # from -M to M
-    js = np.arange(-m, m + 1)
-    coeffs = (delta_t / np.sqrt(2 * np.pi)) * np.exp(-1 * js**2 * (delta_t**2) / 2)
-    times = js * delta_t * np.sqrt(2 * t)
-
-    return coeffs, times
-
-
-class GroundState:
-    def __init__(self, synthesizer: Synthesizer, observable: Hamiltonian, **kwargs):
+class LCUSynthesizer:
+    def __init__(self, synthesizer, observable: Hamiltonian, **kwargs):
         """
         Code for running single ancilla LCU simulation using given synthesizer.
 
