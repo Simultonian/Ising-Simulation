ee35494dc7351a20fe8347315e4f22163e87df79
Zeeshan Ahmed <zeeshan060801@gmail.com>
2024-01-02 18:06:22 +0530

    10x qdrift is better

--- ising/groundstate/simulation/lcusynth.py
+++ ising/groundstate/simulation/lcusynth.py
@@ -107,7 +107,7 @@ class LCUSynthesizer:
 
     def calculate_mu(self):
         p = np.abs(self.lcu_coeffs) / np.linalg.norm(self.lcu_coeffs, ord=1)
-        count = self.ground_params["count"]
+        count = 10 * self.ground_params["count"]
 
         print("Entering loop mu")
         results = []
