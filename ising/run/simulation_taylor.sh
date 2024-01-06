2e861fe8bbd36857a76e0e6eb15f8ca346061811
Zeeshan Ahmed <zeeshan060801@gmail.com>
2024-01-07 00:30:29 +0530

    Taylor plots for simulation `10x`

--- ising/run/simulation_taylor.sh
+++ ising/run/simulation_taylor.sh
@@ -1,3 +1,3 @@
 python simulation/taylor/run_taylor.py --input data/input/taylor.json
 python simulation/exact/run_exact.py --input data/input/taylor.json
-python plotter/time_simulation.py --input data/plotfig/taylor.json
+python plotter/common.py --input data/plotfig/taylor.json
