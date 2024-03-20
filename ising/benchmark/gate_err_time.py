fbfd97d8e0ab8423bb1772485fd92ff4735c6fcf
Zeeshan Ahmed <zeeshan060801@gmail.com>
2024-03-20 16:02:03 +0530

    added another plot

--- ising/benchmark/gate_err_time.py
+++ ising/benchmark/gate_err_time.py
@@ -2,30 +2,37 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-from ising.hamiltonian import parametrized_ising, trotter_reps, qdrift_count
+from ising.hamiltonian import parametrized_ising
 from ising.benchmark.sim_function import (
     taylor_gate_count,
+    trotter_gate_count_ising,
+    qdrift_gate_count,
 )
 
 
-def plot_gate_error(qubit, h_val, error, point_count, obs_norm, start_time, end_time):
+def plot_gate_error(
+    qubit, h_val, start_err_exp, end_err_exp, point_count, obs_norm, time
+):
     fig, ax = plt.subplots()
 
     configs = {
         "taylor": {"color": "blue", "label": "Truncated Taylor"},
-        "trotter": {"color": "black", "label": "First Order Trotter"},
+        # "trotter": {"color": "black", "label": "First Order Trotter"},
         "qdrift": {"color": "red", "label": "qDRIFT Protocol"},
     }
 
-    time_points = [x for x in np.linspace(start_time, end_time, point_count)]
-    ham = parametrized_ising(qubit, h_val)
-    lambd = np.sum(np.abs(ham.coeffs))
-
+    error_points = [
+        10**x for x in np.linspace(start_err_exp, end_err_exp, point_count)
+    ]
     taylor, trotter, qdrift = [], [], []
-    for time in time_points:
+    ham = parametrized_ising(qubit, h_val)
+
+    inv_err = [1 / x for x in error_points]
+
+    for error in error_points:
         taylor.append(taylor_gate_count(ham, time, error, obs_norm))
-        trotter.append(qubit * trotter_reps(qubit, h_val, time, error))
-        qdrift.append(qdrift_count(lambd, time, error))
+        trotter.append(trotter_gate_count_ising(ham, time, error))
+        qdrift.append(qdrift_gate_count(ham, time, error))
 
     results = {"taylor": taylor, "trotter": trotter, "qdrift": qdrift}
 
@@ -33,29 +40,26 @@ def plot_gate_error(qubit, h_val, error, point_count, obs_norm, start_time, end_
         result = results[method]
         sns.lineplot(
             y=result,
-            x=time_points,
+            x=error_points,
             ax=ax,
             label=config["label"],
             color=config["color"],
         )
-
-        sns.scatterplot(y=result, x=time_points, ax=ax, color=config["color"])
+        sns.scatterplot(y=result, x=error_points, ax=ax, color=config["color"])
 
     # SETTING: AXIS VISIBILITY
     ax.spines["top"].set_visible(False)
     ax.spines["right"].set_visible(False)
 
-    # ax.invert_xaxis()
-    # ax.set_xscale("log")
+    ax.invert_xaxis()
+    ax.set_xscale("log")
     ax.set_yscale("log")
-    ax.set_xlabel(r"time ($t$)")
-    ax.set_ylabel(r"Logarithmic Gate Depth")
-
-    # ax.set_title("Depth vs Time for Simulation Techniques")
+    ax.set_xlabel(r"Error ($\log$ scale)")
+    ax.set_ylabel(r"Gate Count ($\log$ scale)")
 
     plt.legend(loc="upper center", bbox_to_anchor=(0.5, 1.10), ncol=3, fontsize=10)
     # plt.legend()
-    diagram_name = "plots/benchmark/gate_time.png"
+    diagram_name = "plots/benchmark/gate_count_err.png"
     print(f"Saving diagram at:{diagram_name}")
     plt.savefig(diagram_name, dpi=300)
 
@@ -63,17 +67,11 @@ def plot_gate_error(qubit, h_val, error, point_count, obs_norm, start_time, end_
 if __name__ == "__main__":
     qubit = 6
     h_val = 0.1
-    start_time, end_time = 1, 20
+    start_err_exp = -1
+    end_err_exp = -5
     point_count = 10
     obs_norm = 1
-    error = 0.1
-
+    time = 20
     plot_gate_error(
-        qubit,
-        h_val,
-        error,
-        point_count,
-        obs_norm,
-        start_time,
-        end_time,
+        qubit, h_val, start_err_exp, end_err_exp, point_count, obs_norm, time
     )
