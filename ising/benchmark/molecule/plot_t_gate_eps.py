c73b4d2cfa8577b20a35814e19f203ba9fb41724
Zeeshan Ahmed <zeeshan060801@gmail.com>
2024-03-24 15:52:01 +0530

    ammonia plots

--- ising/benchmark/molecule/plot_t_gate_eps.py
+++ ising/benchmark/molecule/plot_t_gate_eps.py
@@ -9,13 +9,6 @@ colors: dict[str, str] = {
     "qDRIFT": "red",
 }
 
-markers: dict[str, str] = {
-    "Truncated Taylor Series": "o",
-    "First Order Trotter": "^",
-    "qDRIFT": "x",
-}
-
-
 def get_gate_count_gsp(molecule: str):
     file_name = f"data/tgatecount/{molecule}.json"
     with open(file_name, "r") as file:
@@ -32,25 +25,29 @@ def plot_dictionaries(name: str, depths: dict[str, dict[float, int]]):
 
     for method, data in depths.items():
         errors = list(data.keys())
+        print(errors)
         gates = list(data.values())
         plt.plot(errors, gates, color=colors[method], alpha=0.6)
-        plt.scatter(errors, gates, color=colors[method], label=method, marker=markers[method])
+        plt.scatter(errors, gates, color=colors[method], label=method)
 
-    ax.set_xticks(errors)
-
-    labels = [convert_to_tick(t) for t in errors]
-
-    ax.set_xticklabels(labels)
+    ax.set_xticks([1, 10**0.5, 10**0.2, 10**-1])
+    # labels = [convert_to_tick(t) for t in errors]
+    # ax.set_xticklabels(labels)
 
     # Remove the top and right border
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
 
     # Add labels for each group
-    plt.xlabel(r"$\log(\text{Error})$")
-    plt.ylabel(r"T Gate Count ($\log$ scale)")
+    plt.xlabel(r"Error ($\log_{10}$ scale)")
+    plt.ylabel(r"T Gate Count ($\log_{10}$ scale)")
     # plt.title(f"Method-Wise T-Gate Count vs Error for GSP for {name}")
     ax.set_yscale("log")
+    ax.set_xscale("log")
+    
+    ax.invert_xaxis()
+
+
     plt.legend(loc="upper center", bbox_to_anchor=(0.5, 1.10), ncol=3, fontsize=10)
 
     file_name = f"plots/benchmark/molecule/tgatecount/{name}.png"
@@ -59,9 +56,8 @@ def plot_dictionaries(name: str, depths: dict[str, dict[float, int]]):
 
 
 def main():
-    name = "ising"
+    name = "ammonia"
     gate_dicts = get_gate_count_gsp(name)
-    print(gate_dicts)
     plot_dictionaries(name, gate_dicts)
 
 
