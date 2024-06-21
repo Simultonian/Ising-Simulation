efc2702219937b31fcd8d24de303719836f5dfa4
Zeeshan Ahmed <zeeshan060801@gmail.com>
2024-06-21 15:48:11 +0530

    trace works fine, tested

--- ising/utils/trace.py
+++ ising/utils/trace.py
@@ -1,12 +1,9 @@
 import numpy as np
+from qiskit.quantum_info import partial_trace as qiskit_partial_trace
 
 
-def partial_trace(mat, pos):
-    """
-    Take partial trace of all the Hilbert spaces present at pos (0-indexed)
-    and return the matrix
-    """
-    reshaped_dm = mat.reshape([2, 2, 2, 2])
-    # partial trace the second space
-    reduced_dm = np.einsum("ijkl->jk", reshaped_dm)
-    return reduced_dm
+def partial_trace(rho, indexes):
+    siz = int(np.log2(rho.shape[0]))
+    # reverse
+    new_indexes = [(siz - 1) - ind for ind in indexes]
+    return qiskit_partial_trace(rho, new_indexes).data
--- tests/utils/test_trace.py
+++ tests/utils/test_trace.py
@@ -4,13 +4,15 @@ from qiskit.quantum_info import partial_trace as qiskit_partial_trace, DensityMa
 
 ZERO = [[1], [0]]
 ONE = [[0], [1]]
+BASIS = [ZERO, ONE]
 
 
 def test_oneone():
+    # |11>
     oneone = np.array([0, 0, 0, 1])
     rho_oneone = np.outer(oneone, oneone.conj())
 
-    rho_2 = partial_trace(rho_oneone, [])
+    rho_2 = partial_trace(rho_oneone, [1])
 
     expected = np.array([[0, 0], [0, 1]])
     np.testing.assert_almost_equal(rho_2, expected)
@@ -20,3 +22,27 @@ def test_oneone():
 
     expected = qiskit_partial_trace(rho_oneone, [1])
     np.testing.assert_almost_equal(rho_2, expected)
+
+def test_basis_three_qubits():
+    for psi0 in BASIS:
+        for psi1 in BASIS:
+            for psi2 in BASIS:
+                psi = np.kron(np.kron(psi0, psi1), psi2)
+                rho = np.outer(psi, psi.conj())
+
+                psi_01 = np.kron(psi0, psi1)
+                rho_01 = np.outer(psi_01, psi_01.conj())
+
+                psi_02 = np.kron(psi0, psi2)
+                rho_02 = np.outer(psi_02, psi_02.conj())
+
+                psi_12 = np.kron(psi1, psi2)
+                rho_12 = np.outer(psi_12, psi_12.conj())
+
+                exp_01 = partial_trace(rho, [2])
+                exp_02 = partial_trace(rho, [1])
+                exp_12 = partial_trace(rho, [0])
+
+                np.testing.assert_almost_equal(rho_02, exp_02)
+                np.testing.assert_almost_equal(rho_01, exp_01)
+                np.testing.assert_almost_equal(rho_12, exp_12)
