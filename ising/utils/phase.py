3c252b08b646f294e8752d162ac7f63adc49db3d
Zeeshan Ahmed <zeeshan060801@gmail.com>
2024-06-22 21:56:53 +0530

    before multi-qubit test

--- ising/utils/phase.py
+++ ising/utils/phase.py
@@ -5,11 +5,15 @@ import cmath
 
 def global_phase(a: NDArray):
     # get the phase of the first non-zero value in the matrix
+    phase = 1
     for row in a:
         for x in row:
             if np.abs(x) > 1e-3:
                 theta = cmath.phase(x)
                 # phase is e^{i\theta}
                 phase = np.exp(0 + 1j * theta)
-                # print(x, theta, phase)
-                return phase
+
+                if phase != 1:
+                    return phase
+
+    return phase
