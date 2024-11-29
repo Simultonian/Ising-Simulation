ab0729a430f1bcda57e41f5a298e1daa59797449
Zeeshan Ahmed <zeeshan060801@gmail.com>
2024-11-29 16:29:12 +0530

    no label plot for non-markovian error

--- ising/benchmark/lines/nonmarkovian/plot_nonmark_err_vs_cx.py
+++ ising/benchmark/lines/nonmarkovian/plot_nonmark_err_vs_cx.py
@@ -59,10 +59,10 @@ def test_main():
     ax.invert_xaxis()
 
 
-    file_name = f"plots/benchmark/lines/nonmarkovian/error_{QUBIT_COUNT}.png"
+    file_name = f"plots/benchmark/lines/nonmarkovian/nolabel_error_{QUBIT_COUNT}.png"
 
-    # ax.get_legend().remove()
-    plt.legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=2, fontsize=10)
+    ax.get_legend().remove()
+    # plt.legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=2, fontsize=10)
     plt.savefig(file_name, dpi=300)
     print(f"saved the plot to {file_name}")
 
